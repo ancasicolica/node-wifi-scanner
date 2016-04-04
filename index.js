1fa96c3ed7810f6c4090d54e146727a0dee9161c
Christian Kuster <christian@kusti.ch>
2016-04-04 20:01:15 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+/**
+ * node-wifi-scanner
+ * Created by kc on 04.04.16.
+ */
+
+const fs      = require('fs');
+const exec    = require('child_process').exec;
+// The tools
+const airport = require('./lib/airport');
+const iwlist  = require('./lib/iwlist');
+const nmcli   = require('./lib/nmcli');
+const netsh   = require('./lib/netsh');
+
+var scanner;
+
+// Initializing the tools
+function initTools(callback) {
+  fs.stat(airport.tool, function (err, stats) {
+    if (stats) {
+      return callback(null, airport);
+    }
+
+    fs.stat(iwlist.tool, function (err, stats) {
+      if (stats) {
+        return callback(null, iwlist);
+      }
+
+      fs.stat(nmcli.tool, function (err, stats) {
+        if (stats) {
+          return callback(null, nmcli);
+        }
+
+        fs.stat(netsh.tool, function (err, stats) {
+          if (stats) {
+            return callback(null, netsh);
+          }
+
+          callback(new Error('No scanner found'));
+        });
+      });
+    });
+  });
+}
+
+function scanNetworks(callback) {
+  exec(scanner.cmdLine, function (err, stdout) {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+    scanner.parseOutput(stdout, callback);
+  });
+}
+
+module.exports = {
+  /**
+   * Scan for wifis
+   * @param callback
+   */
+  scan: function (callback) {
+    if (!scanner) {
+      initTools((err, s) => {
+        if (err) {
+          return callback(err);
+        }
+        scanner = s;
+        scanNetworks(callback);
+      });
+      return;
+    }
+    scanner(callback);
+  }
+};
---
+++ tests/airport.js
@@ -0,0 +1,68 @@
+/**
+ * Airport unit test
+ * Created by kc on 04.04.16.
+ */
+
+const fs = require('fs');
+const path = require('path');
+const assert = require('assert');
+
+const airport = require('../lib/airport');
+
+describe('airport', () => {
+  it('parses the output of file 1', function(done) {
+    airport.parseOutput(fs.readFileSync(path.join(__dirname, 'fixtures','airport','airport01.txt'), { encoding: 'utf8' }), (err, info) => {
+
+      assert.ok(info);
+      assert.equal(info.length, 36);
+
+      var ap = info[0];
+      assert.equal(ap.mac, '00:35:1a:90:56:03');
+      assert.equal(ap.ssid, 'OurTest');
+      assert.equal(ap.rssi, -70);
+      assert.strictEqual(ap.channel, 112);
+
+      ap = info[19];
+      assert.equal(ap.mac, '00:35:1a:5b:45:b6');
+      assert.equal(ap.ssid, 'PDANet1');
+      assert.equal(ap.rssi, -78);
+      assert.strictEqual(ap.channel, 11);
+
+      ap = info[25];
+      assert.equal(ap.mac, '10:bd:18:ab:4d:8f');
+      assert.equal(ap.ssid, 'TEST Training');
+      assert.equal(ap.rssi, -71);
+      assert.strictEqual(ap.channel, 6);
+
+      ap = info[35];
+      assert.equal(ap.mac, '00:35:1a:90:56:00');
+      assert.equal(ap.ssid, 'TEST-Wifi');
+      assert.equal(ap.rssi, -67);
+      assert.strictEqual(ap.channel, 1);
+
+      done(err);
+    });
+  });
+
+  it('parses the output of file 2', function(done) {
+    airport.parseOutput(fs.readFileSync(path.join(__dirname, 'fixtures','airport','airport02.txt'), { encoding: 'utf8' }), (err, info) => {
+      console.log(info);
+      assert.ok(info);
+      assert.equal(info.length, 4);
+
+      var ap = info[0];
+      assert.equal(ap.mac, '7c:b7:33:ae:3b:06');
+      assert.equal(ap.ssid, 'Raupo');
+      assert.equal(ap.rssi, -80);
+      assert.strictEqual(ap.channel, 64);
+
+      ap = info[3];
+      assert.equal(ap.mac, '7c:b7:33:ae:3b:04');
+      assert.equal(ap.ssid, 'Visitor Raupo');
+      assert.equal(ap.rssi, -66);
+      assert.strictEqual(ap.channel, 9);
+
+      done(err);
+    });
+  });
+});
